b12e865cb953030fbf3b2587f100a67186f53377
vveennyy <venybylaev@gmail.com>
2020-02-26 04:13:28 +0300

    2

--- vveennyy/vveennyy/Account.cs
+++ vveennyy/vveennyy/Account.cs
@@ -7,5 +7,17 @@ namespace vveennyy
 {
     class Account
     {
+        /// <summary>
+        /// Поле содержащее наименование счёта
+        /// </summary>
+        private string _Nomination { get; set; }
+        /// <summary>
+        /// Поле содержащее номер счёта
+        /// </summary>
+        private int _AccountNumber { get; set; }
+        /// <summary>
+        /// Поле содержащее сумму на счёте
+        /// </summary>
+        private double _Amount { get; set; }
     }
 }
